b5f4029d33c1fa9fa1007c10ab464ab79a4ddd6b
PabloGomezBr <gomezbravopablo@gmail.com>
2022-09-21 20:55:41 +0200

    fix server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+const http = require("http");
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>Tele-g-bot</h1>");
+});
+
+server.listen(port, () => {
+    console.log(`Server running at port ` + port);
+});
+
 const telegramBot = require("node-telegram-bot-api");
 const { createWriteStream } = require("fs");
 
